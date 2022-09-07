cd7edc637e3844dc426c33c1eba8cbf7245cd892
yuvrajcell-99 <yyyuvraj11@gmail.com>
2022-09-07 23:39:59 +0530

    nav-export-link

--- navbar.js
+++ navbar.js
@@ -1,90 +1,50 @@
 export const navbar = () => `
-<div id="nav-container">
-    <div id="navbar">
-      
-        <div>
-          <img src="https://www.expedia.com/_dms/header/logo.svg" alt="" />
-        </div>
-        <div class="drp-dwn">
-          <div class="dropdown">
-            <button onclick="myFunction()" class="dropbtn">
-              More Travel <i class="fa-solid fa-angle-down"></i>
-            </button>
-            <div id="myDropdown" class="dropdown-content">
-              <a href="#"
-                ><box-icon
-                  name="buildings"
-                  animation="burst"
-                  color="#7f7b7c"
-                ></box-icon>
-                &nbsp; Stays</a
-              >
-              <a href="#"
-                ><box-icon
-                  name="plane-alt"
-                  type="solid"
-                  animation="burst"
-                  color="#7f7b7c"
-                ></box-icon>
-                &nbsp; Flight</a
-              >
-              <a href="#"
-                ><box-icon
-                  name="car"
-                  animation="burst"
-                  color="#7f7b7c"
-                ></box-icon>
-                &nbsp; Car</a
-              >
-              <a href="#">
-                <box-icon
-                  name="briefcase-alt"
-                  type="solid"
-                  flip="horizontal"
-                  animation="tada"
-                  color="#7f7b7c"
-                ></box-icon>
-                &nbsp; Packages</a
-              >
-              <a href="#"
-                ><box-icon
-                  name="collection"
-                  animation="burst"
-                  color="#7f7b7c"
-                ></box-icon
-                >&nbsp; Holiday Activites</a
-              >
-              <a href="#">Deals</a>
-              <a href="#">Group & meetings</a>
-              <a href="#">Mobile</a>
-            </div>
-          </div>
-        </div>
+<div id="navbar">
        
-      
+<div>
+ <img src="https://www.expedia.com/_dms/header/logo.svg" alt=""> 
+</div>
+ <div class="drp-dwn">
+
+
+  <div class="dropdown">
+     <button onclick="myFunction()" class="dropbtn"> More Travel <i class="fa-solid fa-angle-down"></i>
+
+     </button>
      
-    
-      <div id="right">
-        <div id="middle">
-          <i class="fa-solid fa-magnifying-glass fa-lg"></i>
-        </div>
-        <div class="tab-nav-bag">
-        <a   href="#" ><i class="fa-solid fa-bag-shopping fa-lg"></i> </a >
-        </div>
-        <div class="desktop-nav">
-        <a href="#" >English </a >
-        <a href="#">Support</a>
-        <a href="#">Trips</a>
-      </div>
-      <div class="static">
-        <a href="#"><i class="fa-solid fa-bell fa-lg"></i></a> 
-        <a href="./signUp.html"><i class="fa-solid fa-user fa-lg"></i></a>
-      </div>
-    </div>
+     <div id="myDropdown" class="dropdown-content">
+         <a  href="#"><box-icon name='buildings' animation='burst' color='#7f7b7c' ></box-icon> &nbsp; Stays</a>
+         <a  href="#"><box-icon name='plane-alt' type='solid' animation='burst' color='#7f7b7c' ></box-icon> &nbsp;  Flight</a>
+         <a  href="#"><box-icon name='car' animation='burst' color='#7f7b7c' ></box-icon> &nbsp; Car</a>
+         <a  href="#"> <box-icon name='briefcase-alt' type='solid' flip='horizontal' animation='tada' color='#7f7b7c' ></box-icon> &nbsp; Packages</a>
+         <a  href="#"><box-icon name='collection' animation='burst' color='#7f7b7c' ></box-icon>&nbsp; Holiday Activites</a>
+         <a  href="#">Deals</a>
+         <a  href="#">Group & meetings</a>
+         <a  href="#">Mobile</a>
+     </div>
    
-    </div>
-    <hr class="line">
-  </div>
+
+ 
+
+ 
+</div>
+ </div>
+ <div id="right">
+   <div id="middle"><i class="fa-solid fa-magnifying-glass"></i></div>
+   <div class="tab-nav-bag"><box-icon name='shopping-bag' type='solid' animation='tada' ></box-icon></div>
+   <div class="desktop-nav">
+     <a href="#"> English</a>
+     <a href="#">Support</a>
+     <a href="#">Trips</a>
+   </div>
+     <div class="static">
+     <a href="#"><box-icon name='bell-ring' type='solid' animation='tada' ></box-icon></a>
+     <a  class="acc" href="./SignIn/index.html"><i class="fa-solid fa-user fa-lg"></i></a>
+ </div>
+</div>
+
+</div>
+<hr class="line">
 
 `
 
